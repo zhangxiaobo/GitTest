ea78771edd2d3049ae0d2b094402f87de92dc696
zhangxiaobo <zhangxiaoboaq0401@163.com>
2016-03-09 11:38:30 +0800

    添加日志标示

--- app/src/main/java/vrv/gittest/MainActivity.java
+++ app/src/main/java/vrv/gittest/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
